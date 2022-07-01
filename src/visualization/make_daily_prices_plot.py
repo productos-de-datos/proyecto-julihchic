36ab5792962a989d06b6b7219509a7b6f952185e
julihchic <101681736+julihchic@users.noreply.github.com>
2022-07-01 11:28:06 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,36 +1,24 @@
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
-
     Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
     lines que representa los precios promedios diarios.
-
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
-
     """
     import pandas as pd
     import matplotlib.pyplot as plt
-
-    
     path_file = r'data_lake/business/precios-diarios.csv'
-
-    
     datos = pd.read_csv(path_file, index_col=None, sep=',', header=0)
     datos["fecha"] = pd.to_datetime(datos["fecha"])
-
-    
     x = datos.fecha
     y = datos.precio
 
-   
     plt.figure(figsize=(15, 6))
-    plt.plot(x, y, 'b', label='Precio Promedio Diario')
-    plt.title('Precios Promedios Diarios')
-    plt.xlabel('Fecha (Años)')
-    plt.ylabel('Precio ($/kWh)')
+    plt.plot(x, y, 'b', label='Promedio Diario')
+    plt.title('Promedio Diario')
+    plt.xlabel('Fecha')
+    plt.ylabel('Precio')
     plt.legend()
     plt.xticks(rotation="vertical")
-
-    
     plt.savefig("data_lake/business/reports/figures/daily_prices.png")
 
     
@@ -38,7 +26,9 @@ def make_daily_prices_plot():
 
 
 if __name__ == "__main__":
+
     import doctest
 
     doctest.testmod()
+    
     make_daily_prices_plot()
