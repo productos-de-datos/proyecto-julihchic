5241fe7b3674e0f738c04c6633350fc67aa13fb7
julihchic <101681736+julihchic@users.noreply.github.com>
2022-07-01 11:27:49 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,38 +1,43 @@
-"""
-Módulo de ingestión de datos.
--------------------------------------------------------------------------------
-
-"""
-
-
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
-
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
-
     """
-    import github
-    import requests
-    g = github.Github()
-    repo = g.get_repo('jdvelasq/datalabs')
-    contents = repo.get_contents('datasets/precio_bolsa_nacional/xls')
+    import pandas as pd
+    import xlwt
 
-    for contentFile in contents:
-        url_descarga = contentFile.download_url
-        nombre_archivo = url_descarga.rsplit('/', 1)[1]
-        with open('data_lake/landing/' + nombre_archivo, 'wb') as f:
-            f.write(requests.get(url_descarga).content)
+    def descargar_archivo(ruta, file_name, extension):
 
+        for año in file_name:
+            url_rute = ruta + '/' + año + extension + "?raw=true"
+            nombre_archivo = "data_lake/landing/" + \
+                '{}{}'.format(año, extension)
+            descarga = pd.read_excel(url_rute)
+            descarga.to_excel("data_lake/landing/{}{}".format(año,
+                                                              extension), index=None, header=True)
+        return
+
+    ruta = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/"
+    file_name1 = [str(año) for año in range(1995, 2016)]
+    file_name2 = [str(año) for año in range(2018, 2022)]
+
+    file_name_xlsx = file_name1 + file_name2
+    descargar_archivo(ruta, file_name_xlsx, ".xlsx")
+
+    file_name_xls = ["2016", "2017"]
+    descargar_archivo(ruta, file_name_xls, ".xls")
+    #raise NotImplementedError("Implementar esta función")
     
 
-    #raise NotImplementedError("Implementar esta función")
+
+
 
 
 if __name__ == "__main__":
+
     import doctest
 
     doctest.testmod()
-
+    
     ingest_data()
