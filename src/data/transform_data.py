16fdaff95a2a85077bccc15cf810a1e4291c90c0
julihchic <101681736+julihchic@users.noreply.github.com>
2022-07-01 11:27:53 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,45 +1,58 @@
+def ruta(year, extension):
+    ruta = "data_lake/landing/{}.{}".format(year, extension)
+    return ruta
+
+
+def load_data(ruta, encabezado):
+    import pandas as pd
+    read_file = pd.read_excel(ruta, header=encabezado)
+    read_file = read_file.iloc[:, 0:25]
+    read_file.columns = ['Fecha', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                         '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+    return read_file
+
+
+def save_data(read_file, year):
+    read_file.to_csv("data_lake/raw/{}.csv".format(year), index=None)
+
+
 def transform_data():
     """Transforme los archivos xls a csv.
-
     Transforme los archivos data_lake/landing/*.xls a data_lake/raw/*.csv. Hay
     un archivo CSV por cada archivo XLS en la capa landing. Cada archivo CSV
     tiene como columnas la fecha en formato YYYY-MM-DD y las horas H00, ...,
     H23.
-
     """
-    import pandas as pd
-
-    
-    encabezado = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
-    
-    
-    for num in range(1995, 2022):        
-        if num in range(1995, 2000):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=3)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado       
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2000, 2016)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2016, 2018)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xls'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
+    for year in range(1995, 2022):
+        if year in range(1995, 2000):
+            out_file = ruta(year, "xlsx")
+            file = load_data(out_file, 3)
+            save_data(file, year)
+        elif(year in range(2000, 2016)):
+            out_file = ruta(year, "xlsx")
+            file = load_data(out_file, 2)
+            save_data(file, year)
+        elif(year in range(2016, 2018)):
+            out_file = ruta(year, "xls")
+            file = load_data(out_file, 2)
+            save_data(file, year)
         else:
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=0)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado            
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
+            out_file = ruta(year, "xlsx")
+            file = load_data(out_file, 0)
+            save_data(file, year)
 
+
+def test_answer():
+    assert ruta('2021', "xlsx") == "data_lake/landing/2021.xlsx"
+
+    
 
     #raise NotImplementedError("Implementar esta función")
+
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    transform_data()
