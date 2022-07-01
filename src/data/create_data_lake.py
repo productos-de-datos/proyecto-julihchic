7ec1239df79caed71daab4e528d37d0fbf732556
julihchic <101681736+julihchic@users.noreply.github.com>
2022-07-01 11:27:46 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,24 +21,24 @@ def create_data_lake():
     
     """
 
-    from os import mkdir
-
-    mkdir('data_lake')
-    mkdir('data_lake/landing')
-    mkdir('data_lake/raw')
-    mkdir('data_lake/cleansed')
-    mkdir('data_lake/business')
-    mkdir('data_lake/business/reports')
-    mkdir('data_lake/business/reports/figures')
-    mkdir('data_lake/business/features')
-    mkdir('data_lake/business/forecasts')
-
-
-
+    import os
+    import os
+    os.mkdir('./data_lake/')
+    parent_dir = 'data_lake/'
+    carpetas = ['landing', 'raw', 'cleansed', 'business']
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
+    parent_dir = 'data_lake/business/'
+    carpetas = ['reports', 'features', 'forecasts']
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
+    parent_dir = 'data_lake/business/reports/'
+    directory = 'figures'
+    os.mkdir(os.path.join(parent_dir, directory))
     #raise NotImplementedError("Implementar esta función")
+    
 
 
 if __name__ == "__main__":
+
     import doctest
 
     doctest.testmod()
