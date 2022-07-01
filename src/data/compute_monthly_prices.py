1120d5b624932dd078133a8a8fd36c71a5f3be26
julihchic <101681736+julihchic@users.noreply.github.com>
2022-07-01 11:27:43 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -1,39 +1,50 @@
 def compute_monthly_prices():
     """Compute los precios promedios mensuales.
-
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
     promedio mensual. Las
     columnas del archivo data_lake/business/precios-mensuales.csv son:
-
     * fecha: fecha en formato YYYY-MM-DD
-
     * precio: precio promedio mensual de la electricidad en la bolsa nacional
-
-
-
     """
-    
     import pandas as pd
+    df = pd.read_csv("data_lake/cleansed/precios-horarios.csv",
+                     index_col=None, header=0)
+    df["fecha"] = pd.to_datetime(df["fecha"])
+    df['year_mes'] = ((df['fecha'].dt.year).astype(int)).astype(
+        str)+"-"+((df['fecha'].dt.month).astype(int)).astype(str)
 
-    
-    data = pd.read_csv("data_lake/cleansed/precios-horarios.csv")
+    df['dia'] = (df['fecha'].dt.day).astype(int)
+    data_para_dia = df[["year_mes", "dia"]]
 
-    data["fecha"] = pd.to_datetime(data["fecha"])
+    dia_agrupacion = data_para_dia.groupby(
+        "year_mes").max({"dia": "dia"})
+    dia_agrupacion.reset_index(inplace=True)
+    dia_agrupacion['fecha'] = dia_agrupacion['year_mes'] + \
+        "-"+(dia_agrupacion['dia']).astype(str)
 
-    
-    data = data.set_index("fecha").resample("M")["precio"].mean()
+    dia_agrupacion["fecha"] = pd.to_datetime(dia_agrupacion["fecha"])
 
-    data.to_csv("data_lake/business/precios-mensuales.csv", index=True)
+    dfm = df[["year_mes", "precio"]]
+    compute_month_prices = dfm.groupby(
+        "year_mes").mean({"precio_promedio": "precio"})
+    compute_month_prices.reset_index(inplace=True)
+
+    compute_month_prices = pd.merge(
+        dia_agrupacion, compute_month_prices, on="year_mes", how="left")
+
+    compute_month_prices = compute_month_prices[["fecha", "precio"]]
+
+    compute_month_prices = compute_month_prices.sort_values(by='fecha')
+    compute_month_prices.to_csv(
+        "data_lake/business/precios-mensuales.csv", index=None, header=True)
 
     #raise NotImplementedError("Implementar esta función")
+    # return
 
-   
-def test_cantidad_meses():
-    import pandas as pd
-    data = pd.read_csv("data_lake/business/precios-mensuales.csv")
-    assert len(data) == 310
 
 if __name__ == "__main__":
+
     import doctest
 
     doctest.testmod()
+    compute_monthly_prices()
